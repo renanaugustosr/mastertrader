f41d382a044a08b8b54fbc2abef464cd00215445
renan augusto <renanaugustosr@gmail.com>
2020-05-11 15:02:20 -0300

    Primeiro commit

---
+++ config/database.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    //url: 'mongodb+srv://renan:renan@cluster0-wgzkn.mongodb.net/test?retryWrites=true&w=majority'
+    url: 'mongodb+srv://mtroot:sokdghnovo@cluster0-e3ttw.mongodb.net/mt?retryWrites=true&w=majority'
+}
+//sokdghnovo
